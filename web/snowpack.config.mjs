f11b34035762b8ff71f56379456e5a19a746cde5
Brooks Rady <b.j.rady@gmail.com>
2022-04-05 22:54:58 +0100

    feat(build): move to snowpack (#12)

---
+++ web/snowpack.config.mjs
@@ -0,0 +1,40 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    '@snowpack/plugin-typescript',
+    [
+      '@snowpack/plugin-svelte',
+      {
+        hmrOptions: {
+          preserveLocalState: true
+        }
+      }
+    ],
+    [
+      'snowpack-plugin-wasm-pack',
+      { projectPath: '../biobox' }
+    ]
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    bundle: true,
+    minify: true,
+    treeshake: true
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
