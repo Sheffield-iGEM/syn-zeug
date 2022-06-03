9ceae8ec86e28964c6178f615f734eab2d45d032
Brooks J Rady <b.j.rady@gmail.com>
2022-06-03 23:18:24 +0100

    feat(bench): added time-complexity measurement

--- syn-zeug/benches/criterion.rs
+++ syn-zeug/benches/criterion.rs
@@ -1,29 +1,38 @@
 mod utils;
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use pprof::criterion::{Output, PProfProfiler};
+use std::time::Duration;
 use syn_zeug::seq::{Seq, SeqKind};
 
-pub fn rev(c: &mut Criterion) {
-    let dna = Seq::dna(utils::load_bench_data("rosalind_dna.txt")).unwrap();
-    c.bench_function("rev 1kb", |b| b.iter(|| dna.rev()));
+fn rev(c: &mut Criterion) {
+    bench_time_complexity(c, "rev", "rosalind_dna.txt", Seq::dna, Seq::rev);
 }
 
-pub fn count_elements(c: &mut Criterion) {
-    let dna = Seq::dna(utils::load_bench_data("rosalind_dna.txt")).unwrap();
-    c.bench_function("count_elements 1kb", |b| b.iter(|| dna.count_elements()));
+fn count_elements(c: &mut Criterion) {
+    bench_time_complexity(
+        c,
+        "count_elements",
+        "rosalind_dna.txt",
+        Seq::dna,
+        Seq::count_elements,
+    );
 }
 
-pub fn dna_to_rna(c: &mut Criterion) {
-    let dna = Seq::dna(utils::load_bench_data("rosalind_rna.txt")).unwrap();
-    c.bench_function("dna_to_rna 1kb", |b| b.iter(|| dna.convert(SeqKind::Rna)));
-}
-
-pub fn reverse_complement(c: &mut Criterion) {
-    let dna = Seq::dna(utils::load_bench_data("rosalind_revc.txt")).unwrap();
-    c.bench_function("reverse_complement 1kb", |b| {
-        b.iter(|| dna.reverse_complement())
+fn dna_to_rna(c: &mut Criterion) {
+    bench_time_complexity(c, "dna_to_rna", "rosalind_dna.txt", Seq::dna, |seq| {
+        seq.convert(SeqKind::Rna)
     });
+}
+
+fn reverse_complement(c: &mut Criterion) {
+    bench_time_complexity(
+        c,
+        "reverse_complement",
+        "rosalind_dna.txt",
+        Seq::dna,
+        Seq::reverse_complement,
+    );
 }
 
 criterion_group!(
@@ -32,3 +41,30 @@ criterion_group!(
     targets = rev, count_elements, dna_to_rna, reverse_complement
 );
 criterion_main!(benches);
+
+fn bench_time_complexity<C, O, R, S, E>(
+    c: &mut Criterion,
+    bench_name: impl Into<String>,
+    data_file: impl AsRef<str>,
+    constructor: C,
+    routine: R,
+) where
+    C: Fn(Vec<u8>) -> Result<S, E>,
+    R: Fn(&S) -> O,
+{
+    let data = utils::load_bench_data(data_file);
+
+    let mut group = c.benchmark_group(bench_name);
+    for p in 0..=10 {
+        let data = data.repeat(2_usize.pow(p));
+        let size = data.len() as u64;
+        if let Ok(seq) = constructor(data) {
+            group.measurement_time(Duration::from_secs(10));
+            group.throughput(Throughput::Bytes(size));
+            group.bench_with_input(BenchmarkId::from_parameter(size), &seq, |b, seq| {
+                b.iter(|| routine(seq));
+            });
+        }
+    }
+    group.finish();
+}
--- syn-zeug/benches/utils.rs
+++ syn-zeug/benches/utils.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
-pub fn load_bench_data(f: &str) -> Vec<u8> {
-    fs::read_to_string(["benches/data/", f].concat())
+pub fn load_bench_data(f: impl AsRef<str>) -> Vec<u8> {
+    fs::read_to_string(["benches/data/", f.as_ref()].concat())
         .unwrap()
         .trim()
         .as_bytes()
