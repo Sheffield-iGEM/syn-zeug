d0ace13b0cb3539e848498099405cb3a1bc3539e
Brooks J Rady <b.j.rady@gmail.com>
2022-10-04 17:50:50 +0100

    Lowercase protein in the input

--- web/src/Main/Input.svelte
+++ web/src/Main/Input.svelte
@@ -8,7 +8,7 @@
     <div class="w-full h-full focus-within:ring-2 ring-primary-500 rounded p-2">
       <textarea
         class="w-full h-full font-plex-mono focus:outline-none resize-none caret-primary-500 !bg-transparent !shadow-none"
-        placeholder="Enter a DNA, RNA, or Protein sequence here..."
+        placeholder="Enter a DNA, RNA, or protein sequence here..."
         spellcheck="false"
         bind:value
       />
